f940fe766670c4205723c4c7ed1d0fc782441bfb
LTN130925 <ltansang374@gmail.com>
2025-08-29 20:14:58 +0700

    thêm tính năng khôi phục lại token chính khi hết hạn sử dụng

--- Backend/src/api/v1/admin/controllers/auth.controller.ts
+++ Backend/src/api/v1/admin/controllers/auth.controller.ts
@@ -63,4 +63,67 @@ export const controller = {
       });
     }
   },
+
+  // [POST] /admin/api/v1/auth/refresh-token
+  refreshToken: async (req: Request, res: Response) => {
+    try {
+      const { refreshToken } = req.body;
+      if (!refreshToken) {
+        return res.status(401).json({
+          success: false,
+          message: 'Vui lòng đăng nhập',
+        });
+      }
+      const decoded = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_SECRET as string
+      ) as any;
+
+      const account = await Account.findOne({
+        _id: decoded.userId,
+        deleted: false,
+        status: 'active',
+      });
+      if (!account) {
+        return res.status(403).json({
+          success: false,
+          message: 'Vui lòng đăng nhập',
+        });
+      }
+
+      const payload: any = {
+        userId: account.id,
+        fullName: account.fullName,
+        status: account.status,
+      };
+      const accessToken = jwt.sign(payload, process.env.SECRET_KEY as string, {
+        expiresIn: '15m',
+      });
+      return res.status(200).json({
+        success: true,
+        message: 'Đăng nhập thành công',
+        accessToken,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: 'Lỗi server',
+      });
+    }
+  },
+
+  // [POST] /admin/api/v1/auth/logout
+  logout: async (req: Request, res: Response) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        message: 'Đăng xuất thành công',
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: 'Loi server',
+      });
+    }
+  },
 };
--- Backend/src/api/v1/admin/routes/auth.route.ts
+++ Backend/src/api/v1/admin/routes/auth.route.ts
@@ -11,4 +11,10 @@ const router = Router();
 // [POST] /admin/api/v1/auth/login
 router.post('/login', loginValidator.login, controller.login);
 
+// [POST] /admin/api/v1/auth/refresh-token
+router.post('/refresh-token', controller.refreshToken);
+
+// [POST] /admin/api/v1/auth/logout
+router.post('/logout', controller.logout);
+
 export default router;
