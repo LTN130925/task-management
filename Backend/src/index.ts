daa75b9def7380ef51adc6e6f092127134be26bc
LTN130925 <ltansang374@gmail.com>
2025-09-12 20:56:41 +0700

    cập nhật thêm địa chỉ cho phép cors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -39,6 +39,7 @@ app.use(bodyParser.json());
 // Cấu hình CORS chi tiết
 // Danh sách origin được phép gọi API
 const allowedOrigins = [
+  'https://task-management-eight-delta.vercel.app',
   'http://localhost:3000',        // local dev
   'https://staging.example.com',  // staging
   'https://example.com',          // production
