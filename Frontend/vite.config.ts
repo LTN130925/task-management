b908a24cd43fceb9d22abcaef8f745c89980a933
LTN130925 <ltansang374@gmail.com>
2025-09-12 17:10:19 +0700

    điều chỉnh lại một số lỗi ngầm để deploy lên vercel

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import env from 'dotenv';
+import dotenv from 'dotenv';
 
-env.config();
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.VITE_PORT as unknown as number,
+    port: Number(process.env.VITE_PORT),
     strictPort: true,
     host: true,
   },
